129dd9d7d44cd3a41718a2a3d066dc7dabed51f4
hunteryavitz <hunteryavitz@protonmail.com>
2023-04-20 20:06:41 -0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,348 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="main">
+    <div class="container">
+      <div style="background-color: #001"></div>
+      <div style="background-color: #002"></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #003"></div>
+      <div style="background-color: #004"></div>
+      <div style="background-color: #005"></div>
+      <div style="background-color: #006"></div>
+      <div style="background-color: #007"></div>
+      <div style="background-color: #008"></div>
+      <div style="background-color: #009"></div>
+      <div style="background-color: #011"></div>
+      <div style="background-color: #011"></div>
+      <div style="background-color: #011"></div>
+      <div style="background-color: #012"></div>
+      <div style="background-color: #013"></div>
+      <div style="background-color: #014"></div>
+      <div style="background-color: #015"></div>
+      <div style="background-color: #016"></div>
+      <div style="background-color: #017"></div>
+      <div style="background-color: #018"></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #019"></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #021"></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #001"></div>
+      <div style="background-color: #002"></div>
+      <div style="background-color: #003"></div>
+      <div style="background-color: #004"></div>
+      <div style="background-color: #005"></div>
+      <div style="background-color: #006"></div>
+      <div style="background-color: #007"></div>
+      <div style="background-color: #008"></div>
+      <div style="background-color: #009"></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #011"></div>
+      <div style="background-color: #012"></div>
+      <div style="background-color: #013"></div>
+      <div style="background-color: #014"></div>
+      <div style="background-color: #015"></div>
+      <div style="background-color: #016"></div>
+      <div style="background-color: #017"></div>
+      <div style="background-color: #018"></div>
+      <div style="background-color: #019"></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #021"></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div></div>
+      <div style="background-color: #001"></div>
+      <div style="background-color: #002"></div>
+      <div style="background-color: #003"></div>
+      <div style="background-color: #004"></div>
+      <div style="background-color: #005"></div>
+      <div style="background-color: #006"></div>
+      <div style="background-color: #007"></div>
+      <div style="background-color: #008"></div>
+      <div style="background-color: #009"></div>
+      <div style="background-color: #011"></div>
+      <div style="background-color: #012"></div>
+      <div style="background-color: #013"></div>
+      <div style="background-color: #014"></div>
+      <div style="background-color: #015"></div>
+      <div style="background-color: #016"></div>
+      <div style="background-color: #017"></div>
+      <div style="background-color: #018"></div>
+      <div style="background-color: #019"></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #021"></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div style="background-color: #fff"></div>
+      <div></div>
+      <div style="background-color: #fff"></div>
+      <div></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div style="background-color: #fff"></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
 
 export default defineComponent({
   name: "App",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 });
+
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.main {
+  display: flex;
+  --s: 100px; /* size  */
+  --m: 4px; /* margin */
+  --f: calc(1.732 * var(--s) + 4 * var(--m) - 1px);
+}
+
+.container {
+  font-size: 0; /*disable white space between inline block element */
+}
+
+.container div {
+  width: var(--s);
+  margin: var(--m);
+  height: calc(var(--s) * 1.1547);
+  display: inline-block;
+  font-size: initial;
+  clip-path: polygon(0% 25%, 0% 75%, 50% 100%, 100% 75%, 100% 25%, 50% 0%);
+  /*set the background to a random color with random opacity*/
+  background: rgba(1, 1, 1, 0.5);
+  margin-bottom: calc(var(--m) - var(--s) * 0.2885);
+}
+.container div:nth-child(odd) {
+  background: rgba(4, 4, 4, 0.3);
+}
+.container::before {
+  content: "";
+  width: calc(var(--s) / 2 + var(--m));
+  float: left;
+  height: 120%;
+  shape-outside: repeating-linear-gradient(
+    #0000 0 calc(var(--f) - 3px),
+    #000 0 var(--f)
+  );
 }
 </style>
